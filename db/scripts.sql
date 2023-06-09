641831adb632830a5e5bcfea019772442954ea09
BrianTuillier <tuillierbrian333@gmail.com>
2023-06-09 10:16:22 -0400

    Agregamos links entre las paginas

--- db/scripts.sql
+++ db/scripts.sql
@@ -16,6 +16,10 @@ select * from options;
 
 insert into options_has_question (question_id_question, options_id_option) values (1,1),(1,2),(1,3),(1,4),(1,5);
 insert into options_has_question (question_id_question, options_id_option) values (2,6), (2, 7);
+insert into options_has_question (question_id_question, options_id_option) values (3,1),(3,2),(3,3),(3,4),(3,5);
+insert into options_has_question (question_id_question, options_id_option) values (4,1),(4,2),(4,3),(4,4),(4,5);
+insert into options_has_question (question_id_question, options_id_option) values (5,1),(5,2),(5,3),(5,4),(5,5);
+insert into options_has_question (question_id_question, options_id_option) values (6,1),(6,2),(6,3),(6,4),(6,5);
 
 select * from options_has_question;
 
